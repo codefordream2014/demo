e5db6d2780512b4bcf790c5aa1245736affb9c57
codefordream2014 <1079078997@qq.com>
2016-03-06 14:53:09 +0800

    uncomment
    umcomment

--- src/com/eric/main/Main.java
+++ src/com/eric/main/Main.java
@@ -6,12 +6,12 @@ import javax.tools.ToolProvider;
 public class Main {
 
 	public static void main(String[] args) throws Exception{
-		/*JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		int result = compiler.run(null, null, null, "C:\\Users\\Eric\\Desktop\\Hello.java");
 		System.out.println(result == 0? "success":"fail");*/
 		
-		/*Runtime runtime = Runtime.getRuntime();
-		Process process = runtime.exec("javac C:\\Users\\Eric\\Desktop\\Hello.java");*/
+		Runtime runtime = Runtime.getRuntime();
+		Process process = runtime.exec("javac C:\\Users\\Eric\\Desktop\\Hello.java");
 		System.out.println(Main.class.getClassLoader());
 		System.out.println(String.class.getClassLoader());
 	}
