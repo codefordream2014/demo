8aa4d6ff58007ec0abb09cf1bdc0835930d89369
Eric Zhang <zhangzhaoy@foxmail.com>
2016-03-06 13:50:00 +0800

    first commit

---
+++ src/com/eric/main/Main.java
@@ -0,0 +1,19 @@
+package com.eric.main;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		/*JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		int result = compiler.run(null, null, null, "C:\\Users\\Eric\\Desktop\\Hello.java");
+		System.out.println(result == 0? "success":"fail");*/
+		
+		/*Runtime runtime = Runtime.getRuntime();
+		Process process = runtime.exec("javac C:\\Users\\Eric\\Desktop\\Hello.java");*/
+		System.out.println(Main.class.getClassLoader());
+		System.out.println(String.class.getClassLoader());
+	}
+
+}
