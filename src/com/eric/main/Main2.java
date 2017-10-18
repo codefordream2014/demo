c1c21350f8ebf4893939696bd8404ba1e7043350
root <root@example.com>
2017-10-18 23:12:19 +0800

    add new lines in release

--- src/com/eric/main/Main2.java
+++ src/com/eric/main/Main2.java
@@ -18,5 +18,7 @@ public class Main {
 		System.out.println(String.class.getClassLoader());
 		System.out.println(String.class.getClassLoader());
 		System.out.println(String.class.getClassLoader());
+		System.out.println(String.class.getClassLoader());
+		System.out.println(String.class.getClassLoader());
      }   
 }
