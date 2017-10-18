d3c0756788c23918b4ea976fb1f59d15997b57e4
root <root@example.com>
2017-10-18 23:14:16 +0800

    test add a new liine

--- src/com/eric/main/Main.java
+++ src/com/eric/main/Main.java
@@ -20,6 +20,7 @@ public class Main {
 		System.out.println(String.class.getClassLoader());
 		System.out.println(String.class.getClassLoader());
 		System.out.println(String.class.getClassLoader());
+		System.out.println(String.class.getClassLoader());
 	}
 
 }
