a9290086a2d72bccb4933dd3271c258bee38cc6c
Eric Zhang <zhangzhaoy@foxmail.com>
2017-12-17 16:45:08 +0800

    change the code
    change the code

--- src/com/eric/main/Main.java
+++ src/com/eric/main/Main.java
@@ -20,11 +20,6 @@ public class Main {
 		System.out.println(String.class.getClassLoader());
 		System.out.println(String.class.getClassLoader());
 		System.out.println(String.class.getClassLoader());
-		System.out.println(String.class.getClassLoader());
-		System.out.println(String.class.getClassLoader());
-		System.out.println(String.class.getClassLoader());
-		System.out.println(String.class.getClassLoader());
-		System.out.println(String.class.getClassLoader());
 		System.out.println(String.class.getClassLoader
                 System.out.println(String.class.getClassLoader());
 		System.out.println(String.class.getClassLoader());
